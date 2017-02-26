57ab9f628015f94fb115067d1df5a4b4e30d0fa9
Cristi Socea <office@soceacristian.ro>
2017-02-26 15:59:07 +0200

    Update indentation of .jsx file

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 
 class App extends React.Component {
-  render () {
-    return <p>It works!</p>;
-  }
+    render () {
+        return <p>It works!</p>;
+    }
 }
 
 render(<App/>, document.getElementById('app'));
